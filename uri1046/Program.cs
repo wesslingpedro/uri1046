c394068f69a2f339f780d20a20fa18841bcf61eb
wesslingpedro <wesslingpedro@gmail.com>
2024-05-21 19:28:39 -0300

    Primeiro commit

---
+++ uri1046/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_ec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int hinicial = int.Parse(valores[0]);
+            int hfinal = int.Parse(valores[1]);
+
+            int duracao;
+
+            if (hinicial < hfinal)
+            {
+                duracao = hfinal - hinicial;
+            }
+            else
+            {
+                duracao = 24 - hinicial + hfinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+
+
+
+
+        }
+
+    }
+
+}
